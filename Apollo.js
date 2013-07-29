0d5162dc3c1c1b861fd904785357422eebaa45de
bobbybee <admin@magnesiumbeta.com>
2013-07-29 18:16:36 -0400

    Rudimentary protocol support, data parsing

--- Apollo.js
+++ Apollo.js
@@ -3,6 +3,17 @@ var network = require('./network'); // network module is a wrapper around the
 
 var config = require('./config'); // configuration loader
 
+var protocol = require('./protocol'); // provides methods for handling the Apollo protocol
+
 network.createServer(function(conn){
-	console.log("Client Connected");
+	console.log("Client connected");
+
+	conn.on('data', function(d){
+		var message = protocol.parsePacket(d); // parse the message
+		console.log(message); // dump the message
+	});
+
+	conn.on('end', function(){
+		console.log("Client disconnected");
+	});
 }).listen(config.port, config.host);
---
+++ protocol.js
@@ -0,0 +1,38 @@
+// protocol.js
+// defines the Apollo messaging protocol
+
+// currently, as of version 0.0.1, Apollo uses an extremely verbose protocol
+// packets are stored in their literal JSON form
+// compressed or full binary packets are planned for future
+
+// parsePacket
+// parses a given packet
+// arguments: packet
+// packet: string representation of packet
+// return value:
+// object form of packet
+
+function parsePacket(packet){
+	try{
+		return JSON.parse(packet);
+	} catch(e){
+		console.log("Encountered error parsing packet");
+		console.log(e);
+		return {"type":"error", "message":e};
+	}	
+}
+
+// generatePacket
+// generates a string represetnation of a packet
+// arguments: obj
+// obj: object represting packet
+// return value:
+// string form of packet
+function generatePacket(obj){
+	return JSON.stringify(obj);
+}
+
+
+// deal with module exports
+module.exports.parsePacket = parsePacket;
+module.exports.generatePacket = generatePacket;
