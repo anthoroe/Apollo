291a756f84299136e10b3034f2700f6ac0a32bb4
bobbybee <admin@magnesiumbeta.com>
2013-07-30 14:05:33 -0400

    Added a more complex extension for movement (showing practical applications)

--- extension.js
+++ extension.js
@@ -1,13 +1,46 @@
-var API;
+// a simple example for an avatar chat
 
-function init(_API){
-	API = _API; // store the API reference
+var users = [];
+var ids = 0;
+
+function broadcast(message){
+	var i = 0;
+	while(i < users.length){
+		users[i].send(message);
+		++i;
+	}
+}
+
+function connect(client, message){
+	users.push(client);
 	
-	API.addMessageListener("ping", ping); // add a listener for the ping message
+	client.id = ids++;
+	
+	broadcast({"type":"connect", "id": client.id});
 }
 
-function ping(client, message){
-	client.send({"type":"pong"});
+function disconnect(client, message){
+	// find user in array
+	var i = 0;
+	while(i < users.length){
+		if(users[i].id == client.id)
+			break;
+		++i;
+	}
+	
+	// remove user
+	users.splice(i);
+	
+	broadcast({"type":"disconnect", "id": client.id});
 }
 
-module.exports.init = init; // expose our init method
+function move(client, message){
+	broadcast({"type":"move", "id":client.id, "x":message.x, "y":message.y});
+}
+
+module.exports.init = function(API){
+	API.addMessageListener("connect", connect);
+	API.addMessageListener("move", move); // add event listeners
+	API.addMessageListener("disconnect", disconnect);
+	
+}; // expose our init method
